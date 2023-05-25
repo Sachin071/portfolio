e3016bf1736de784f1bc65d149e203f4f8bf09e3
Shashankcognic <shashank.patel@cognicsys.com>
2023-05-25 12:42:44 +0530

    first portfolio

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Work from "./Pages/Work";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
