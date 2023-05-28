f6f391414f66cd0febc8a348b02e2e1c2484ac19
Shashankcognic <shashank.patel@cognicsys.com>
2023-05-28 17:19:46 +0530

    navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import About from "./Pages/About";
-import Contact from "./Pages/Contact";
 import Home from "./Pages/Home";
 import Work from "./Pages/Work";
 
@@ -13,7 +12,6 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/work" element={<Work />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,63 +1,60 @@
-import React, { useRef, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import "../../assets/css/navbar.css";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdCall } from "react-icons/md";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  const mobile_nav_ref = useRef(null);
-  const nav_header_ref = useRef(null);
-  const [cls, setCLS] = useState("active");
-
-  const toggleNavbar = () => {
-    setCLS("");
-  };
-
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
   return (
-    <header className={`header ${cls}`} ref={nav_header_ref}>
-      <p className="logo">Shashank Patel</p>
-      <nav className="navbar">
-        <ul className="navbar-list">
-          <li>
-            <NavLink className="navbar-link" to="/" title="Shashank-Home">
-              <i className="fa fa-home" aria-hidden="true" />
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className="navbar-link"
-              to="/work"
-              title="Shashank-work-experience"
-            >
-              <i className="fa fa-building" aria-hidden="true"></i> Project
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="navbar-link" to="/about" title="Shashank-About">
-              <i className="fa fa-user" aria-hidden="true"></i> About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="navbar-link" to="/contact" title="Contact-me">
-              <i className="fa fa-volume-control-phone" aria-hidden="true"></i>{" "}
-              Contact
-            </NavLink>
-          </li>
-        </ul>
+    <>
+      <nav className="main-nav">
+        {/* 1st logo part  */}
+        <div className="logo">
+          <h2>
+            <span>S</span>hashank &nbsp;
+            <span>P</span>atel
+          </h2>
+        </div>
+
+        {/* 2nd menu part  */}
+        <div
+          className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }
+        >
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">about</NavLink>
+            </li>
+            <li>
+              <NavLink to="/work">Projet</NavLink>
+            </li>
+          </ul>
+        </div>
+
+        {/* 3rd social media links */}
+        <div className="social-media">
+          <ul className="social-media-desktop">
+            <li>
+              <a href="tel:830-523-1235">
+                <MdCall />
+              </a>
+            </li>
+          </ul>
+
+          {/* hamburget menu start  */}
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
+        </div>
       </nav>
-      <div className="mobile-navbar-btn">
-        <i
-          className="fa-solid fa-bars mobile-nav-icon"
-          name="bars"
-          onClick={() => setCLS("active")}
-        ></i>
-        <i
-          className="fa-sharp fa-solid fa-xmark mobile-nav-icon"
-          onClick={toggleNavbar}
-          name="xmark"
-        ></i>
-      </div>
-    </header>
+    </>
   );
 };
 
